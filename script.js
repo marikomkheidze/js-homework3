c31f2fb07cb27b493bb16e5ee10a0ffeec3bb0a0
marikomkheidze <110020576+marikomkheidze@users.noreply.github.com>
2022-09-28 15:35:12 +0400

    amocana 1

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+"use strict"
 // ამოცანა 1
+
+let array  = [5, 25, 89, 120, 36]
+let sum=0
+array.push ("javascript", "python")
+console.log(array);
+array.unshift ("html", "css")
+console.log(array);
+console.log();
+array.shift ()
+console.log(array);
+array.pop()
+console.log(array);
+
 // ამოცანა 2
 // ამოცანა 3
 // ამოცანა 4 
@@ -6,12 +20,20 @@
 // ამოცანა 6
 // ამოცანა 7
 // ამოცანა 8
+
+
+
+
 // ამოცანა 9 
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0
+// arr.forEach(item => {
+//     sum+=item
+// })
+// console.log(sum);
 
 
-
-
+ 
 
 // ამოცანა 10 
 // ამოცანა 11
